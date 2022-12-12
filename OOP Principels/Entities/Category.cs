cfb3fd978f6c7b72d9095d9c7cb994ca17ee6438
iktueren <iktuerensemih@gmail.com>
2022-12-12 23:58:57 +0300

    Add project files.

---
+++ OOP Principels/Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Principels.Entities
+{
+    internal class Category
+    {
+        void NameControl(string name)
+        {
+            if(name.Length<5)
+            {
+                throw new Exception("Category name connot be longer than 5 character");
+
+            }
+                
+        }
+
+        public Category(string categoryname)
+        {
+            NameControl(categoryname);
+            CategoryName = categoryname;
+        }
+    }
+}
---
+++ OOP Principels/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Principels.Models
+{
+    public class Product
+    {
+
+        decimal _unitprice;
+
+        public decimal UnitPrice
+        {
+            get
+            {
+                if(_unitprice<5)
+                {
+                    System.Windows.Forms.MessageBox.Show($"Please be aware ! the unit price{_unitprice} cannot be lower than 5, the value will be revised to minimum value of 5 automatically");
+
+                }
+                return _unitprice = 5;
+            }
+            set
+            {
+                if(value<5)
+                {
+                    throw new Exception("Price canot be lower than 5!");
+                }
+                _unitprice = value;
+            }
+        }
+    }
+}
